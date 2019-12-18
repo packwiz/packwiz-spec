e4522b466a5efeaf7dfba3bc9fad2eb0b1e32c30
comp500 <comp500@users.noreply.github.com>
2019-12-18 01:33:10 +0000

    Add node build command instead of powershell

--- genSchemas.ps1
+++
@@ -1,4 +0,0 @@
-typescript-json-schema typings.d.ts Pack --strictNullChecks -o schemas/pack.json --required
-typescript-json-schema typings.d.ts Index --strictNullChecks -o schemas/index.json --required
-
-# typedoc --out test/ typings.d.ts --mode file --toc "Pack,Index" --includeDeclarations --excludeExternals
