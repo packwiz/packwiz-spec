7dda8fa04f700e42b6a62236069456a1a433e458
comp500 <comp500@users.noreply.github.com>
2023-02-04 20:55:08 +0000

    Fix pattern compat with Taplo

--- src/defs/shared/Path.ts
+++ src/defs/shared/Path.ts
@@ -7,6 +7,6 @@ export class Path implements SchemaGenerator {
 		description:
 			"A relative path using forward slashes. Must not reference a file outside the pack root, and should not include characters or filenames restricted on common operating systems.\n\nImplementations must support special characters including those that are percent-encoded in URLs, such as spaces and square brackets. Implementations must guard against path traversal attacks and manually validate paths.",
 		// Ban characters not allowed in NTFS filenames, and empty path components
-		pattern: `^([^/\\0-\\x1F\\x7F"*/:<>?\\\\|]+\\/)*[^/\\0-\\x1F\\x7F"*/:<>?\\\\|]*$`,
+		pattern: `^([^/\\x00-\\x1F\\x7F"*/:<>?\\\\|]+\\/)*[^/\\x00-\\x1F\\x7F"*/:<>?\\\\|]*$`,
 	};
 }
